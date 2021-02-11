9a72d015cf442379e71fbaac7f134303cd8168a5
Storm K <asge0907@easv365.dk>
2021-02-11 08:37:34 +0100

    Setup passport, began using express session and injected ejs to view

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,47 @@
-import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import express from 'express';
-import { sanitizedUser } from './models/user';
-import startRoute from './routes';
+import session from 'express-session';
+import passport from 'passport';
 import postRoute from './routes/post';
 import userRoute from './routes/user';
-
-const prisma = new PrismaClient();
+import { isLoggedIn } from './utils/passport';
 
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 // Set express to use url encoded extended property. TLDR allows nested objects. & the responses to only be parsed as JSON
 app.use(express.urlencoded({ extended: true }), express.json());
+app.use(session({ secret: process.env.SECRET!, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('', startRoute);
+//Routes
+// app.use('', startRoute); removed because EJS was added
 app.use('/users', userRoute);
 app.use('/posts', postRoute);
 
-app.use('/users', async (_req, res) => {
-  const users: sanitizedUser[] = await prisma.user.findMany({
-    select: {
-      email: true,
-      displayName: true,
-      createdAt: true,
-      updatedAt: true,
-      posts: { select: { title: true } },
-    },
-  });
+app.get('/', (req, res) => {
+  res.render('home', { user: req.user });
+});
 
-  return res.json(users);
+app.get('/home', (req, res) => {
+  res.render('home', { user: req.user });
+});
+app.get('/login', (_req, res) => {
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login', successRedirect: '/profile' }));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { user: req.user });
 });
 
 const server = app.listen(process.env.PORT, () => {
