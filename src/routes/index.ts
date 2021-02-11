80e83a9130290e7cca72c03b3e4a5240f2f5d810
Storm K <asge0907@easv365.dk>
2021-02-11 07:32:21 +0100

    fixed typo on landing page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,9 @@ const startRoute = Router();
 startRoute.get('/', async (_req, res) => {
   return res.json({
     0: 'Hello and welcome.',
-    1: 'This API is built up of the following: Users and Posts.',
-    2: 'You can query users on /users and posts on /posts.',
-    3: 'You can also get posts belonging to a user by using "/users/[id]/posts',
+    1: 'This API is built up of the following: Users and Posts .',
+    2: 'You can query users on /users and posts on /posts .',
+    3: 'You can also get posts belonging to a user by using /users/[id]/posts .',
     4: 'Enjoy and have fun!',
   });
 });
