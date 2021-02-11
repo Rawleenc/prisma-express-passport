6699c6888623ed06ec8f124481228ea87659a5ae
Storm K <asge0907@easv365.dk>
2021-02-11 13:06:45 +0100

    Added register route and logic

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
+import argon2id from 'argon2';
 import { Router } from 'express';
 import passport from 'passport';
+import { createDb } from '../utils/db';
 import { isLoggedIn } from './../utils/passport';
 
+const db = createDb();
 const startRoute = Router();
 
 startRoute.get('', (req, res) => {
@@ -16,6 +19,10 @@ startRoute.get('/login', (_req, res) => {
   res.render('login');
 });
 
+startRoute.get('/register', (_req, res) => {
+  res.render('register');
+});
+
 startRoute.post('/login', passport.authenticate('local', { failureRedirect: '/login', successRedirect: '/profile' }));
 
 startRoute.get('/logout', (req, res) => {
@@ -25,6 +32,21 @@ startRoute.get('/logout', (req, res) => {
 
 startRoute.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { user: req.user });
+});
+
+startRoute.post('/register', async (req, res) => {
+  await db.user
+    .create({
+      data: {
+        email: req.body.email,
+        password: await argon2id.hash(req.body.password),
+        displayName: req.body.displayName,
+      },
+    })
+    .then(_user => {
+      res.redirect('/login');
+    })
+    .catch(() => res.status(400).json('Unable to add user'));
 });
 
 export default startRoute;
