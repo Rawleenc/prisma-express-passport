5955eb19a1d8db32de058e3448c62f30cc67dbcd
Storm K <asge0907@easv365.dk>
2021-02-11 01:47:53 +0100

    Integrated prisma, responds with posts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import express from 'express';
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 // Set express to use url encoded extended property. TLDR allows nested objects. & the responses to only be parsed as JSON
 app.use(express.urlencoded({ extended: true }), express.json());
+
+app.use('/', async (_req, res) => {
+  res.json(await prisma.post.findMany());
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log('Server ready & listening to http://localhost:' + process.env.PORT);
