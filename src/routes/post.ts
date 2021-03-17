eacb6a4215048b8162b1f7e8fe802cfdf44d8a19
Storm K <asge0907@easv365.dk>
2021-03-17 20:38:32 +0100

    added update post

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -47,7 +47,28 @@ postRoute.get('/:id', async (req, res) => {
 //#endregion
 
 //#region UPDATE
+postRoute.put('/:id', isLoggedIn, async (req, res) => {
+  const { id } = req.params;
 
+  const post = await db.post.findUnique({ where: { id: parseInt(id) }, include: { author: true } });
+  if (!post) return res.status(404).json('Unable to find post');
+
+  const reqUser = req.user as User;
+
+  if (post.author.id !== reqUser.id) return res.status(403).json("You don't have permission to update this");
+
+  db.post
+    .update({
+      where: { id: parseInt(id) },
+      data: {
+        title: req.body.title,
+        content: req.body.content,
+        published: req.body.published,
+      },
+    })
+    .then(post => res.status(200).json(post))
+    .catch(_err => res.status(400).json('Unable to delete post'));
+});
 //#endregion
 
 //#region DELETE
