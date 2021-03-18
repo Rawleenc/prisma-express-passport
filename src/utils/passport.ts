10a85384ef5b66b062e626bdd99cf68dae50e694
Storm K <asge0907@easv365.dk>
2021-03-18 05:40:00 +0100

    Added documentation to passport

--- src/utils/passport.ts
+++ src/utils/passport.ts
@@ -5,11 +5,15 @@ import { Strategy } from 'passport-local';
 import { User } from '../models/user';
 import prisma from './db';
 
-const db = prisma;
-
+/**
+ * Authentication strategy (Username & Password)
+ * Since we are using email and password we have to rename the username field to what we use.
+ * Attempts to find a user in the db, will approve or reject based on user found and if password matches or not.
+ * Hashed using Argon2ID
+ */
 passport.use(
   new Strategy({ usernameField: 'email' }, async (email, password, done) => {
-    const user = await db.user.findUnique({ where: { email } });
+    const user = await prisma.user.findUnique({ where: { email } });
     const response = 'Invalid login credentials';
 
     if (!user) return done(response);
@@ -23,12 +27,19 @@ passport.use(
   }),
 );
 
+/**
+ * Serializes the user, called after login is approved.
+ * accesses the user object, resulting in data attached to the session. (Request.session.user)
+ */
 passport.serializeUser((user, done) => {
   done(null, (user as User).id);
 });
 
+/**
+ * Deserialize the user, on every session request through the used passport strategy. (Local / email & password)
+ */
 passport.deserializeUser(async (id: string | number, done) => {
-  const user = await db.user.findUnique({ where: { id: parseInt(id.toString()) } });
+  const user = await prisma.user.findUnique({ where: { id: parseInt(id.toString()) } });
   if (!user) return done('No user to deserialize');
 
   return done(null, user);
@@ -36,6 +47,7 @@ passport.deserializeUser(async (id: string | number, done) => {
 
 /**
  * Login Required middleware.
+ * NOTE Consider showing either an error, or simply redirect the user to log in page
  */
 export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
   if (req.isAuthenticated()) return next();
