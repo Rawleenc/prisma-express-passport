70237f0de2e9d7786d9a958992e2d14b8e012aa0
Storm K <asge0907@easv365.dk>
2021-03-18 09:51:37 +0100

    Updated code (added joi-validation) and added documentation

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,19 +1,27 @@
 import { Router } from 'express';
+import { sanitizedUser } from '../models/user';
 import prisma from '../utils/db';
-import { User } from './../models/user';
-import { isLoggedIn } from './../utils/passport';
+import { isLoggedIn } from '../utils/passport';
+import { postSchema } from './../models/joi';
 
 const postRoute = Router();
 const db = prisma;
 
 //#region CREATE
+
+/**
+ * Registers a post title and content
+ */
 postRoute.post('/', isLoggedIn, async (req, res) => {
+  const { error } = postSchema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0] });
+
   db.post
     .create({
       data: {
         title: req.body.title,
         content: req.body.content,
-        author: { connect: { id: (req.user as User).id } },
+        author: { connect: { email: (req.user as sanitizedUser).email } },
       },
     })
     .then(post => res.status(200).json(post))
@@ -22,40 +30,59 @@ postRoute.post('/', isLoggedIn, async (req, res) => {
 //#endregion
 
 //#region READ
+
+/**
+ * Get all posts
+ */
 postRoute.get('/', async (_req, res) => {
   // imitate sanitizedPost by only selecting the displayName from the author on the post.
   const posts = await db.post.findMany({
     include: { author: { select: { displayName: true } } },
   });
 
+  if (!posts) return res.status(404).json('Unable to find posts');
+
   res.json(posts);
 });
 
+/**
+ * Gets a post by id.
+ */
 postRoute.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   // imitate sanitizedPost by only selecting the displayName from the author on the post.
-  const posts = await db.post.findUnique({
+  const post = await db.post.findUnique({
     where: { id: parseInt(id) },
     include: {
       author: { select: { displayName: true } },
     },
   });
 
-  res.json(posts);
+  if (!post) return res.status(404).json('Unable to find post');
+
+  res.json(post);
 });
 //#endregion
 
 //#region UPDATE
+
+/**
+ * Updates a post, only works if logged in. Requires the body to contain
+ * "title" (string), "content" (string), optional "published" (boolean)
+ */
 postRoute.put('/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
+
+  const { error } = postSchema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0] });
 
   const post = await db.post.findUnique({ where: { id: parseInt(id) }, include: { author: true } });
   if (!post) return res.status(404).json('Unable to find post');
 
-  const reqUser = req.user as User;
+  const { email } = req.user as sanitizedUser;
 
-  if (post.author.id !== reqUser.id) return res.status(403).json("You don't have permission to update this");
+  if (post.author?.email !== email) return res.status(403).json("You don't have permission to update this");
 
   db.post
     .update({
@@ -72,15 +99,18 @@ postRoute.put('/:id', isLoggedIn, async (req, res) => {
 //#endregion
 
 //#region DELETE
+/**
+ * Deletes a post, only works if logged in and user is author of post
+ */
 postRoute.delete('/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
 
   const post = await db.post.findUnique({ where: { id: parseInt(id) }, include: { author: true } });
   if (!post) return res.status(404).json('Unable to find post');
 
-  const reqUser = req.user as User;
+  const { email } = req.user as sanitizedUser;
 
-  if (post.author.id !== reqUser.id) return res.status(403).json("You don't have permission to delete this");
+  if (post.author?.email !== email) return res.status(403).json("You don't have permission to delete this");
 
   db.post
     .delete({ where: { id: parseInt(id) } })
