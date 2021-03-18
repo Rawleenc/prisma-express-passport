3749802993a1c344fe71f15facac60694ff41e07
Storm K <asge0907@easv365.dk>
2021-03-18 09:40:06 +0100

    added update to user and documentation

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,35 +1,52 @@
+import argon2id from 'argon2';
 import { Router } from 'express';
+import { registerSchema } from '../models/joi';
 import prisma from '../utils/db';
+import { isLoggedIn } from '../utils/passport';
 import { sanitizedUser } from './../models/user';
 
 const userRoute = Router();
 const db = prisma;
 
-userRoute.get('/', async (_req, res) => {
+//Creating user is handled in index.ts (Register)
+
+//#region READ
+
+/**
+ * Get all users (as sanitized users, stripping away some data. Shown in models/user)
+ */
+userRoute.get('/', async (req, res) => {
   const users: sanitizedUser[] = await db.user.findMany({
     select: {
       email: true,
       displayName: true,
       createdAt: true,
       updatedAt: true,
-      posts: { select: { title: true } },
     },
+  });
+
+  // Hide other users email from request
+  users.forEach(u => {
+    const user = req.user as sanitizedUser | undefined;
+    if (u.email !== user?.email) u.email = '';
   });
 
   return res.json(users);
 });
 
+/**
+ * Gets a specific user if possible
+ */
 userRoute.get('/:id', async (req, res) => {
   const { id } = req.params;
 
-  const user = await db.user.findUnique({
+  const user: sanitizedUser | null = await db.user.findUnique({
     where: { id: parseInt(id) },
     select: {
       email: true,
       displayName: true,
       createdAt: true,
       updatedAt: true,
-      posts: { select: { title: true } },
     },
   });
 
@@ -37,6 +54,9 @@ userRoute.get('/:id', async (req, res) => {
   else res.json(user);
 });
 
+/**
+ * Gets the posts of a specific user if possible
+ */
 userRoute.get('/:id/posts', async (req, res) => {
   const { id } = req.params;
 
@@ -54,5 +74,40 @@ userRoute.get('/:id/posts', async (req, res) => {
 
   res.json(posts);
 });
+//#endregion
 
+//#region UPDATE
+
+/**
+ * Updatess a user, only works if logged in and user is themself
+ */
+userRoute.put('/:id', isLoggedIn, async (req, res) => {
+  const { id } = req.params;
+
+  const { error } = registerSchema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0] });
+
+  const user = await db.user.findUnique({ where: { id: parseInt(id) } });
+  if (!user) return res.status(404).json('Unable to find user');
+
+  const { email } = req.user as sanitizedUser;
+  if (user.email !== email) return res.status(403).json("You don't have permission to update this");
+
+  db.user
+    .update({
+      where: { id: parseInt(id) },
+      data: {
+        email: req.body.email,
+        password: await argon2id.hash(req.body.password),
+        displayName: req.body.displayName,
+      },
+    })
+    .then(post => res.status(200).json(post))
+    .catch(_err => res.status(400).json('Unable to delete post'));
+});
+//#endregion
+
+//#region DELETE
+
+//#endregion
 export default userRoute;
