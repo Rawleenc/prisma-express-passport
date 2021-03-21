83e584e96839d2d987d68b3635ad90c90ab50fac
Storm K <asge0907@easv365.dk>
2021-03-21 01:51:09 +0100

    removed static text values from file

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,7 @@
 import argon2id from 'argon2';
 import { Router } from 'express';
+import path from 'path';
+import { Actions, Responses } from '../utils/constants';
 import prisma from '../utils/db';
 import { isLoggedIn } from '../utils/passport';
 import { userSchema } from './../models/joi';
@@ -7,6 +9,9 @@ import { sanitizedUser } from './../models/user';
 
 const userRoute = Router();
 const db = prisma;
+const type = path.basename(__filename.split('.')[0]);
+const types = type + 's';
+const types2 = 'posts';
 
 //Creating user is handled in index.ts (Register)
 
@@ -25,6 +30,8 @@ userRoute.get('/', async (req, res) => {
     },
   });
 
+  if (!users) return res.status(404).json(Responses.read.none_found(types));
+
   // Hide other users email from request
   users.forEach(u => {
     const user = req.user as sanitizedUser | undefined;
@@ -39,6 +46,7 @@ userRoute.get('/', async (req, res) => {
  */
 userRoute.get('/:id', async (req, res) => {
   const { id } = req.params;
+  if (isNaN(parseInt(id))) return res.status(400).json(Responses.invalid_id(type));
 
   const user: sanitizedUser | null = await db.user.findUnique({
     where: { id: parseInt(id) },
@@ -50,8 +58,8 @@ userRoute.get('/:id', async (req, res) => {
     },
   });
 
-  if (!user) res.json({ error: 'Unable to find user' });
-  else res.json(user);
+  if (!user) return res.status(404).json(Responses.read.none_found(type));
+  res.json(user);
 });
 
 /**
@@ -59,6 +67,7 @@ userRoute.get('/:id', async (req, res) => {
  */
 userRoute.get('/:id/posts', async (req, res) => {
   const { id } = req.params;
+  if (isNaN(parseInt(id))) return res.status(400).json(Responses.invalid_id(type));
 
   const posts = await db.post.findMany({
     where: { userId: parseInt(id) },
@@ -72,6 +81,8 @@ userRoute.get('/:id/posts', async (req, res) => {
     },
   });
 
+  if (!posts) return res.status(404).json(Responses.read.none_found(types2));
+
   res.json(posts);
 });
 //#endregion
@@ -83,15 +94,16 @@ userRoute.get('/:id/posts', async (req, res) => {
  */
 userRoute.put('/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
+  if (isNaN(parseInt(id))) return res.status(400).json(Responses.invalid_id(type));
 
   const { error } = userSchema.validate(req.body);
   if (error) return res.status(400).json({ error: error.details[0] });
 
   const user = await db.user.findUnique({ where: { id: parseInt(id) } });
-  if (!user) return res.status(404).json('Unable to find user');
+  if (!user) return res.status(404).json(Responses.read.none_found(type));
 
   const { email } = req.user as sanitizedUser;
-  if (user.email !== email) return res.status(403).json("You don't have permission to update this");
+  if (user.email !== email) return res.status(403).json(Responses.no_permissions('update'));
 
   db.user
     .update({
@@ -103,7 +115,7 @@ userRoute.put('/:id', isLoggedIn, async (req, res) => {
       },
     })
     .then(post => res.status(200).json(post))
-    .catch(_err => res.status(400).json('Unable to delete post'));
+    .catch(_err => res.status(400).json(Responses.unable_to_perform('update', type)));
 });
 //#endregion
 
@@ -115,13 +127,14 @@ userRoute.put('/:id', isLoggedIn, async (req, res) => {
  */
 userRoute.delete('/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
+  if (isNaN(parseInt(id))) return res.status(400).json(Responses.invalid_id(type));
 
   const user = await db.user.findUnique({ where: { id: parseInt(id) } });
-  if (!user) return res.status(404).json('Unable to find user');
+  if (!user) return res.status(404).json(Responses.read.none_found(type));
 
   const { email } = req.user as sanitizedUser;
 
-  if (user.email !== email) return res.status(403).json("You don't have permission to delete this");
+  if (user.email !== email) return res.status(403).json(Responses.unable_to_perform(Actions.delete, type));
 
   db.user
     .delete({ where: { id: parseInt(id) } })
@@ -132,8 +145,9 @@ userRoute.delete('/:id', isLoggedIn, async (req, res) => {
     })
     .catch(_err => {
       console.log(_err);
-      res.status(400).json('Unable to delete user');
+      res.status(400).json();
     });
 });
 //#endregion
+
 export default userRoute;
