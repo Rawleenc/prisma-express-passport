e924db00dc037bec1a9e54070c51f1436205cda7
Storm K <asge0907@easv365.dk>
2021-04-01 04:19:56 +0200

    simplified test file, now using npm scripts

--- prisma/prisma-test-environment.js
+++ prisma/prisma-test-environment.js
@@ -4,12 +4,7 @@ const util = require('util');
 const NodeEnvironment = require('jest-environment-node');
 const exec = util.promisify(require('child_process').exec);
 
-const prismaBinary = path.join(__dirname, '..', 'node_modules', '.bin', 'prisma');
-
 class PrismaTestEnvironment extends NodeEnvironment {
-  dbName;
-  dbPath;
-
   /**
    * @param {import("@jest/types/build/Config").ProjectConfig} config
    */
@@ -25,17 +20,14 @@ class PrismaTestEnvironment extends NodeEnvironment {
 
   async setup() {
     // Run the migrations to ensure our schema has the required structure
-    await exec(`${prismaBinary} db push --preview-feature`);
-    await exec(`${prismaBinary} db seed --preview-feature`);
+    await exec(`yarn migrate`);
+    await exec(`yarn seed`);
+
     return super.setup();
   }
 
   async teardown() {
-    try {
-      await fs.promises.unlink(this.dbPath);
-    } catch (error) {
-      // doesn't matter as the environment is torn down
-    }
+    await fs.promises.unlink(this.dbPath);
   }
 }
 
